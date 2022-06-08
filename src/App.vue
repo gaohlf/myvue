84475823af54e9ba473433a1dc29678829ac26b7
ghl <20075113ghl@sina.com>
2022-06-08 09:53:06 +0800

    初始工程

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <header_icon msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import header_icon from './components/header_icon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    header_icon
   }
 }
 </script>
---
+++ src/components/header_icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header_icon">
+    <div style="float: left">
+      <el-button  type="info" @click="test()">测试</el-button>
+    </div>
+      <div style="float: right">
+        <img id="service" src="../imag/service.png" style="" width="40" height="40" alt=""/>
+        <img id="disk" src="../imag/disk.png" width="40" height="40" alt=""/>
+        <img id="server" src="../imag/server.png" width="40" height="40" alt=""/>
+        <img id= "net" src="../imag/net.png" width="40" height="40" alt=""/>
+        <img id="soft" src="../imag/soft.png" width="40" height="40" alt=""/>
+      </div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "header_icon",
+  methods:{
+    test()
+    {
+      alert("hello");
+    }
+  }
+}
+
+</script>
+
+test()
+{
+  alert("hello");
+}
+
+<style scoped>
+
+</style>
